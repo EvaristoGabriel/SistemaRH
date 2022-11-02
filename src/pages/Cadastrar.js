055c59ddc3d51010969b72ebf883d6a9d091b487
GabrielEvaristo <gabriel.evaristo@estudante.ufjf.br>
2022-11-02 11:18:21 -0300

    campos cadatro

--- src/pages/Cadastrar.js
+++ src/pages/Cadastrar.js
@@ -3,16 +3,38 @@ import styles from "../components/layout/Cadastrar.module.css"
 import { Button } from "@mui/material"
 import styleButton from "../components/layout/StyleButton.module.css"
 
+function cadastro(valor) {
+    return (
+        <form key={valor}>
+            <input name="name" autoComplete="off" required className={styles.input} />
+            <label className={styles.label} htmlFor="name">
+                <span className={styles.span}>{valor}</span>
+            </label>
+        </form>
+    )
+}
+
 function Cadastrar() {
+    const campos = [
+        { id: 1, text: "Nome"},
+        { id: 2, text: "Data de Nascimento"},
+        { id: 3, text: "Nacionalidade"},
+        { id: 3, text: "Naturalidade"},
+        { id: 3, text: "Grau de Instrução"},
+        { id: 3, text: "Estado Civil"},
+        { id: 3, text: "Sexo"},
+        { id: 3, text: "Raça"},
+      ];
     return (
         <div className="App">
             <header className="App-header">
                 Cadastro
             </header>
             <br></br>
-            <div className={styles.menu}>
+            {campos.map((campo) =>(
+                cadastro(campo.text)
+            ))}
             <Button class={styleButton.button} href="/menu" size="medium">Voltar</Button>
-            </div>
         </div>
     )
 }
