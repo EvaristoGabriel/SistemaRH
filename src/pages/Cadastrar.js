f6d8751d655ca243eb93b0fc18219fa083852199
EvaristoGabriel <gabriel.evaristo@estudante.ufjf.br>
2022-11-02 21:27:52 -0300

    att

--- src/pages/Cadastrar.js
+++ src/pages/Cadastrar.js
@@ -4,13 +4,15 @@ import { Button } from "@mui/material"
 import styleButton from "../components/layout/StyleButton.module.css"
 
 function cadastro(valor) {
-    return (
-        <form key={valor}>
-            <input name="name" autoComplete="off" required className={styles.input} />
-            <label className={styles.label} htmlFor="name">
-                <span className={styles.span}>{valor}</span>
-            </label>
-        </form>
+    return ( 
+        <div className={styles.menu}>
+            <form key={valor}>
+                <input name="name" autoComplete="off" required className={styles.input} />
+                <label className={styles.label} htmlFor="name">
+                    <span className={styles.span}>{valor}</span>
+                </label>
+            </form>
+        </div>
     )
 }
 
@@ -19,11 +21,13 @@ function Cadastrar() {
         { id: 1, text: "Nome"},
         { id: 2, text: "Data de Nascimento"},
         { id: 3, text: "Nacionalidade"},
-        { id: 3, text: "Naturalidade"},
-        { id: 3, text: "Grau de Instrução"},
-        { id: 3, text: "Estado Civil"},
-        { id: 3, text: "Sexo"},
-        { id: 3, text: "Raça"},
+        { id: 4, text: "Naturalidade"},
+        { id: 5, text: "Grau de Instrução"},
+        { id: 6, text: "Estado Civil"},
+        { id: 7, text: "Sexo"},
+        { id: 8, text: "Raça"},
+        { id: 9, text: "Filiação"},
+        { id: 10, text: "CPF"},
       ];
     return (
         <div className="App">
@@ -31,14 +35,10 @@ function Cadastrar() {
                 Cadastro
             </header>
             <br></br>
-<<<<<<< HEAD
-            <Button class={styleButton.button} href="/menu" size="small">Voltar</Button>
-=======
             {campos.map((campo) =>(
                 cadastro(campo.text)
-            ))}
-            <Button class={styleButton.button} href="/menu" size="medium">Voltar</Button>
->>>>>>> 055c59ddc3d51010969b72ebf883d6a9d091b487
+                ))}
+            <Button class={styleButton.button} href="/menu" size="small">Voltar</Button>
         </div>
     )
 }
